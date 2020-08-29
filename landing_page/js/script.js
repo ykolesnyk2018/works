a402bc560ad4e5bb686d1e931d098db741eb50d9
ykolesnyk2018 <69803424+ykolesnyk2018@users.noreply.github.com>
2020-08-29 23:17:19 +0300

    4

--- landing_page/js/script.js
+++ landing_page/js/script.js
@@ -23,7 +23,7 @@ $( document ).ready(function(){
         e.preventDefault();
         $('body').toggleClass('mobile-menu-active');
     });
-    $('.up, .main-menu ul li a').click( function(){ // ловим клик по ссылке с классом go_to
+    $('.up, .main-menu ul li a, .footer-nav ul li a').click( function(){ // ловим клик по ссылке с классом go_to
 	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
         if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
 	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000); // анимируем скроолинг к элементу scroll_el

0e61f51cda037176dacc90fbf51e5826963daae0
ykolesnyk2018 <69803424+ykolesnyk2018@users.noreply.github.com>
2020-10-18 11:43:40 +0300

    dsds

---
+++ gulpds/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	$(function() {
+		$('select.styler').styler();
+  	});
+	/* popUp */
+    $('.show_popup').click(function() {
+		var popup_id = $('#' + $(this).attr("rel"));
+		$(popup_id).show();
+		$('.overlay_popup').show();
+	}); 
+	$('.overlay_popup').click(function() {
+		$('.overlay_popup, .popup').hide();
+	});
+	/* popUp */
+    $(function() {
+        $(".class-name").click(function(){
+            $(this).toggleClass("active");
+        }); 
+    });
+    $(function(){
+        $(".tabs").tabs();
+    });
+	$('.one-time').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+	  adaptiveHeight: true
+	});
+});

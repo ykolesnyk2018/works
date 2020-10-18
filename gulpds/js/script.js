0df7b2c8b9d9e27ec9a0c6a5aca341c5b440e636
ykolesnyk2018 <69803424+ykolesnyk2018@users.noreply.github.com>
2020-10-18 22:21:36 +0300

    dfdfd

--- gulpds/js/script.js
+++ gulpds/js/script.js
@@ -1,30 +1,83 @@
 $(document).ready(function(){
 	$(function() {
-		$('select.styler').styler();
+		$('select').styler();
   	});
+	/* toggle */
+    $('.toggle').click(function() {
+		var toggle_id = $('#' + $(this).attr("data-target"));
+		$(this).toggleClass("active");
+		$(toggle_id).toggleClass("active");
+	}); 
+	/* toggle */
 	/* popUp */
     $('.show_popup').click(function() {
-		var popup_id = $('#' + $(this).attr("rel"));
+		var popup_id = $('#' + $(this).attr("data-target"));
 		$(popup_id).show();
-		$('.overlay_popup').show();
+		$('.overlay').show();
 	}); 
-	$('.overlay_popup').click(function() {
-		$('.overlay_popup, .popup').hide();
+	$('.overlay').click(function() {
+		$('.overlay, .popup').hide();
 	});
 	/* popUp */
+	$(function(){
+		var availableTags = [
+		  "Зарядка JD233",
+		  "Зарядка JD233 и аккумулятор",
+		  "Зарядка JD233 и аккумулятор",
+		  "Зарядка JD233 и аккумулятор",
+		  "Зарядка JD233 и аккумулятор",
+		  "Зарядка JD233 и аккумулятор",
+		  "Зарядка JD233 и аккумулятор",
+		  "COBOL",
+		  "ColdFusion",
+		  "Erlang",
+		  "Fortran",
+		  "Groovy",
+		  "Haskell",
+		  "Java",
+		  "JavaScript",
+		  "Lisp",
+		  "Perl",
+		  "PHP",
+		  "Python",
+		  "Ruby",
+		  "Scala",
+		  "Scheme"
+		];
+		$("#tags").autocomplete({
+		  source: availableTags
+		});
+	 });
+    $('.close-autocomplete').click(function() {
+		$("#tags").autocomplete("close");
+	}); 
+	$('.main-slider').slick({
+		arrows: true,
+		dots: true,
+	    infinite: true,
+	    slidesToShow: 1,
+	    slidesToScroll: 1
+	});
+	$('.feedbacks-slider').slick({
+	  dots: true,
+	  speed: 300,
+	    slidesToScroll: 1,
+  	  adaptiveHeight: true,
+	  variableWidth: true,
+	  responsive: [
+		{
+		  breakpoint: 768,
+		  settings: {
+		  }
+		}
+	  ]
+	});
+	
+	
+	
     $(function() {
         $(".class-name").click(function(){
             $(this).toggleClass("active");
         }); 
     });
-    $(function(){
-        $(".tabs").tabs();
-    });
-	$('.one-time').slick({
-	  dots: true,
-	  infinite: true,
-	  speed: 300,
-	  slidesToShow: 1,
-	  adaptiveHeight: true
-	});
 });

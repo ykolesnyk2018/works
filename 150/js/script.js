982aca51086914769596c7f6cd27dca0c6dc2346
ykolesnyk2018 <69803424+ykolesnyk2018@users.noreply.github.com>
2020-11-10 15:32:43 +0200

    fdfd

---
+++ 150/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$(function() {
+		$('select.styler').styler();
+  	});
+	/* popUp */
+    $('.show_popup').click(function() {
+		var popup_id = $('#' + $(this).attr("rel"));
+		$(popup_id).show();
+		$('.overlay_popup').show();
+	}); 
+	$('.overlay_popup').click(function() {
+		$('.overlay_popup, .popup').hide();
+	});
+	/* popUp */
+    $(function() {
+        $(".class-name").click(function(){
+            $(this).toggleClass("active");
+        }); 
+    });
+    $(function(){
+        $(".tabs").tabs();
+    });
+	$('.one-time').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+	  adaptiveHeight: true
+	});
+	$('.main-slider').slick({
+	    slidesToShow: 1,
+	    slidesToScroll: 1,
+		dots: true
+	});
+});

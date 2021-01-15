6d1883df92b54378296b1c401ca4c3c53ed5801e
ykolesnyk2018 <69803424+ykolesnyk2018@users.noreply.github.com>
2021-01-15 16:14:18 +0200

    da

---
+++ 60l/js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	$(function() {
+		$('select.styler').styler();
+  	});
+	/* popUp */
+    $('.show_popup').click(function() {
+		var popup_id = $('#' + $(this).attr("rel"));
+		$(popup_id).show();
+		$('.overlay_popup').show();
+	}); 
+	$('.overlay_popup').click(function() {
+		$('.overlay_popup, .popup').hide();
+	});
+	/* popUp */
+    $(function() {
+        $(".class-name").click(function(){
+            $(this).toggleClass("active");
+        }); 
+    });
+    $(function(){
+        $(".tabs").tabs();
+    });
+        $('.clients-slider').slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true
+    });
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+      arrows: false,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: false,
+      focusOnSelect: true,
+      variableWidth: true
+    });
+
+    /* toggle */
+    $('.toggle').click(function() {
+        var toggle_id = $('#' + $(this).attr("data-target"));
+        $(this).toggleClass("active");
+        $(toggle_id).toggleClass("active");
+      }); 
+      /* toggle */
+});
